d02940f14c0bab15b8eef4198da759dffc45f7ae
RTAI Linux Local User <rtai@pcl4105c.esiee.fr>
2015-10-07 18:20:32 +0200

    sens_task periodically executed every 1 sec

--- controller/controller.c
+++ controller/controller.c
@@ -1,4 +1,4 @@
-// 0.0.0 2015-08-07
+// 0.0.1 2015-08-07
 
 
 #include<linux/init.h>
@@ -16,30 +16,54 @@ MODULE_LICENSE("GPL");
 
 // Scheduling parameters
 #define STACK_SIZE  2000
-#define PERIOD      100000000   //  100 ms // MATLAB peeriod = 0.010
+//#define PERIOD      100000000   //  100 ms // MATLAB peeriod = 0.010
+#define PERIOD      1000000000   //  1s // for debug
 #define TICK_PERIOD 1000000     //  1 ms
+#define NB_LOOP     10          // number of times the task are executed
 
 static RT_TASK sens_task, act_task, pid_task;
+RTIME now; // tasks start time
 
 int raw2mRad(int raw_value) {
   return 3.845e-1 * raw_value - 782;
 }
 
 void senscode(int arg) {
-  printk("[sens_task]\n");
+  static int loop = NB_LOOP;
+  RTIME t, t_old;
+
+  while (loop--) {
+    t = rt_get_time();
+    printk("[sens_task] time: %llu ns\n", count2nano(t - now));
+    /* sensor acquisition code */
+
+    /* end of sensor acquisition code */
+    t_old = t;
+    rt_task_wait_period();
+  }
 }
 
 void actcode(int arg) {
-  printk("[act_task]\n");
+  static int loop = NB_LOOP;
+  RTIME t, t_old;
+
+  while (loop--) {
+    t = rt_get_time();
+    printk("[act_task] time: %llu ms\n", count2nano(t - now));
+    /* controller code */
+
+    /* end of controller code */
+    t_old = t;
+    rt_task_wait_period();
+  }
 }
 
 static int test_init(void) {
   int ierr;
-  RTIME now;
 
   rt_set_oneshot_mode();
 
-  printk("\033[32mPGM STARTING\033[0m\n");
+  printk("PGM STARTING\n");
 
   ierr = rt_task_init_cpuid(&sens_task,  // task
                             senscode,    // rt_thread

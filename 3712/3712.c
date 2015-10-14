1ca0a0dd5f1d265fa478e8ad67f24df8a2443701
Guillaume Legrain <legrainguillaume@gmail.com>
2015-10-14 21:03:57 +0200

    code cleanup

--- 3712/3712.c
+++ 3712/3712.c
@@ -1,4 +1,4 @@
-// 0.0.3 2015-09-30
+// 0.0.4 2015-10-14
 // 3712.c
 // Guillaume Legrain <guillaume.legrain@edu.esiee.fr>
 // Florian Martin <florian.martin@edu.esiee.fr>
@@ -7,6 +7,7 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+MODULE_LICENSE("GPL");
 
 #include <asm/io.h>
 #include <linux/pci.h>
@@ -34,19 +35,19 @@ void setDA_raw(int, int);
  * Driver initilisation
  * Reset output to 0V and enable DAC
  */
-static int init_3712(void) { 
+static int init_3712(void) {
+    printk("install 3712 driver\n");
 
-	printk("install 3712 driver\n");
-	// reset output value for both channel to 0V
-	setDA_mVolt(1,0);
-	setDA_mVolt(2,0);
+    // reset output value for both channel to 0V
+    setDA_mVolt(1,0);
+    setDA_mVolt(2,0);
 
-	//enable output
-	outb(0xFF, OUTPUT_CTRL);
+    //enable output
+    outb(0xFF, OUTPUT_CTRL);
 
-	return(0);	
-
-}//init_3712
+    return(0);
+}
+EXPORT_SYMBOL(init_3712);
 
 /**
  * Set output voltage using raw register values
@@ -55,26 +56,26 @@ static int init_3712(void) {
  */
 void setDA_raw(int channel, int value){
 
-  // First 8 LSB (D7-D0) from value
-	char value_low = value & 0xFF;
-	// On récupère de D8 à D11
-  // Remaining 8 MSB from value
-	char value_high = value >> 8;
-	
-  // Write desired value into registers based on selected channel
-	if (channel == 1) {
-		outb(value_low, DA_LOW_BYTE_CH1);
-		outb(value_high, DA_HIGH_BYTE_CH1);
-	} else if (channel == 2) {
-		outb(value_low, DA_LOW_BYTE_CH2);
-		outb(value_high, DA_HIGH_BYTE_CH2);
-	} else {
-		printk("ERROR: invalid channel");
-		return;
-	}
-	//update DAC output value // Looks like this has no effect
-	//outb(0xFF, SYNC_TRANS_CTRL);
-}//setDA
+    // First 8 LSB (D7-D0) from value
+    char value_low = value & 0xFF;
+    // Remaining 8 MSB from value
+    char value_high = value >> 8;
+
+    // Write desired value into registers based on selected channel
+    if (channel == 1) {
+        outb(value_low, DA_LOW_BYTE_CH1);
+        outb(value_high, DA_HIGH_BYTE_CH1);
+    } else if (channel == 2) {
+        outb(value_low, DA_LOW_BYTE_CH2);
+        outb(value_high, DA_HIGH_BYTE_CH2);
+    } else {
+        printk("ERROR: invalid channel");
+        return;
+    }
+    //update DAC output value // Looks like this has no effect
+    //outb(0xFF, SYNC_TRANS_CTRL);
+}
+EXPORT_SYMBOL(setDA_raw);
 
 /**
  * Set output voltage in mVolts
@@ -82,39 +83,33 @@ void setDA_raw(int channel, int value){
  * @arg int value_mVolt desired voltage (+/- 10000mV)
  */
 void setDA_mVolt(int channel, int value_mVolt) {
-  // Limit voltage to +/- 10V
-	if (value_mVolt >= 10000) {
-		setDA_raw(channel, 0xFFF);
-		return;
-	}
-	if (value_mVolt <= -10000) {
-		setDA_raw(channel, 0x000);
-		return;
-	}
-	
-	int value = (2.048*value_mVolt / VREF) + 2048;
-	setDA_raw(channel, value);	
+    // Limit voltage to +/- 10V
+    if (value_mVolt >= 10000) {
+        setDA_raw(channel, 0xFFF);
+        return;
+    }
+    if (value_mVolt <= -10000) {
+        setDA_raw(channel, 0x000);
+        return;
+    }
+
+    // convert mVolt value to register value
+    int value = (2.048*value_mVolt / VREF) + 2048;
+    setDA_raw(channel, value);
 }
+EXPORT_SYMBOL(setDA_mVolt);
 
 /**
  * Uninstall driver module
  * Disable DAC output
  */
 void exit_3712(void) {
-	
-	//disable output
-	outb(0x00, OUTPUT_CTRL);
-	printk("uninstall 3712 driver\n");
+    //disable output
+    outb(0x00, OUTPUT_CTRL);
+    printk("uninstall 3712 driver\n");
 
 }
-
-
-// Export function for use in other modules
-EXPORT_SYMBOL(setDA_mVolt);
-EXPORT_SYMBOL(setDA_raw);
-EXPORT_SYMBOL(init_3712);
 EXPORT_SYMBOL(exit_3712);
-
 
 module_init(init_3712);
 module_exit(exit_3712);
--- 3718/3718.c
+++ 3718/3718.c
@@ -1,4 +1,4 @@
-// 0.0.3 2015-09-30
+// 0.0.4 2015-10-14
 // 3718.c
 // Guillaume Legrain <guillaume.legrain@edu.esiee.fr>
 // Florian Martin <florian.martin@edu.esiee.fr>
@@ -7,6 +7,7 @@
 
 #include <linux/init.h>
 #include <linux/module.h>
+MODULE_LICENSE("GPL");
 
 #include <asm/io.h>
 #include <linux/pci.h>
@@ -37,80 +38,85 @@ void setChannel(int in_channel);
 void ADRangeSelect(int channel, int range);
 u16 readAD(void);
 double readAD_mVolt(void);
+void configure_pacer(void);
 
 
 /**
  * Driver initilisation
  */
-static int init_3718(void) { 
+static int init_3718(void) {
+    printk("install 3718 driver\n");
 
-	printk("install 3718 driver\n");
-	
-  // CONTROL REGISTER
-  // D7: INTE: Enable/Disable interrupts
-  // D6-4 Enable/Disable interrupt INL0, INL1, INL3
-  // D3: N/A
-  // D2: DMAE: Enable DMA
-  // D1-D0: Trigger source: Sfwr: 0X, Ext: 10, Pacer: 11
-	outb(0xFF,CONTROL);
-	
-	// Program counters 1 and 2 as pacers
-  // to generate A/D conversions trigger pulses
-	// 25kHz
+    // CONTROL REGISTER
+    // D7: INTE: Enable/Disable interrupts
+    // D6-4 Enable/Disable interrupt INL0, INL1, INL3
+    // D3: N/A
+    // D2: DMAE: Enable DMA
+    // D1-D0: Trigger source: Sfwr: 0X, Ext: 10, Pacer: 11
+    outb(0xFF,CONTROL);
 
-  // PACER ENABLE REGISTER
-  // TC0: Enable/Disable pacer
-   outb(0x00, PACER);
-	
-	// COUNTER CONTROL
-	// Set Counter 1 to mode 3
-	outb(0x76 , COUNTER_CTRL); 
+    configure_pacer();
 
-	// COUNTER 1
-	// Write low Byte of C1
-	outb(40, COUNTER1);
-	// Write high Byte of C1	
-	outb(0, COUNTER1);
-	
-	// COUNTER CONTROL
-	// Set Counter 2 to mode 3
-	outb(0xB6 , COUNTER_CTRL);
+    return(0);
+}
+EXPORT_SYMBOL(init_3718);
 
-	// COUNTER 2
-	// Write low byte of C2
-	outb(10, COUNTER2);
-	// Write high byte of C2
-	outb(0, COUNTER2);
-	
-	return(0);	
+/**
+ * Configure pacer
+ * to generate A/D conversions trigger pulses.
+ * Program counters 1 and 2 as pacers
+ * 25kHz
+ */
+void configure_pacer(void) {
+    // PACER ENABLE REGISTER
+    // TC0: Enable/Disable pacer
+    outb(0x00, PACER);
 
-}//init_3718
+    // COUNTER CONTROL
+    // Set Counter 1 to mode 3
+    outb(0x76 , COUNTER_CTRL);
+    // COUNTER 1
+    // Write low Byte of C1
+    outb(40, COUNTER1);
+    // Write high Byte of C1
+    outb(0, COUNTER1);
+
+    // COUNTER CONTROL
+    // Set Counter 2 to mode 3
+    outb(0xB6 , COUNTER_CTRL);
+    // COUNTER 2
+    // Write low byte of C2
+    outb(10, COUNTER2);
+    // Write high byte of C2
+    outb(0, COUNTER2);
+}
 
 /**
  * Set channel to perform A/D conversion
  * @arg int channel (0-3)
  */
 void setChannel(int in_channel) {
-  // Disable channel scanning by setting the start and stop channel
-  // to the same value
-  // 4 MSB = scan stop; 4 LSB = start scan
-  char value = (in_channel<<4) + in_channel;
-  outb(value , MUX_CTRL);
-  // Write anything to BASE to trigger A/D conversion
-  // removed because a/D conversion is trigger by the pacer
-  //outb(0x1,BASE);
+    // Disable channel scanning by setting the start and stop channel
+    // to the same value
+    // 4 MSB = scan stop; 4 LSB = start scan
+    char value = (in_channel<<4) + in_channel;
+    outb(value , MUX_CTRL);
+    // Write anything to BASE to trigger A/D conversion
+    // removed because A/D conversion is trigger by the pacer
+    //outb(0x1,BASE);
 
-  // wait for channel change
-  //u8 result_low_channel= inb(AD_LOW_BYTE_AND_CH) & 0x7;
-  //while ((int)result_low_channel != in_channel) {
+    // wait for channel change
+    //u8 result_low_channel= inb(AD_LOW_BYTE_AND_CH) & 0x7;
+    //while ((int)result_low_channel != in_channel) {
     //printk("waiting for channel change\n");
     //result_low_channel = inb(AD_STATUS_REGISTER) & 0x7;
-  //} // Not working, prink or delay hack works ?????
-  // printk("Channel changed\n");
-  int i = 0;
-  for (i = 0; i < 100000; i++); // delay hack to wait for channel change
+    //} // Not working, prink or delay hack works ?????
+    // printk("Channel changed\n");
+    int i = 0;
+    for (i = 0; i < 100000; i++); // delay hack to wait for channel change
 
-}//SetChanel
+}
+EXPORT_SYMBOL(setChannel);
 
 /**
  * Set input range and channel
@@ -118,71 +124,78 @@ void setChannel(int in_channel) {
  * @arg int range see datasheet for values; 8 for +/- 10V
  */
 void ADRangeSelect(int channel, int range) {
-	setChannel(channel);
-	// on passe au channel suivant lorsqu'on écrit dans AD_RANGE_CTRL
-	outb(range, AD_RANGE_CTRL);
-	//on reset channel
-        // reset channel //TODO: test on order of setChannel
-	//setChannel(channel);
-
-}//ADRangeSelect
-
-u16 readAD(void) {
-	// STATUS REGISTER
-	// D7: End Of Conversion:
-        //   1 Busy,
-        //   0 Ready for next conversion data from the previous
-        //     conversion is available in the A/D register
-	// D6: N/A
-	// D5: MUX: 0 8 Diff channels; 1 16 Single Ended Channels
-	// D4: INT Data valid
-        // D3-D0: CN3-CN0 When EOC = 0,
-        //
-        // these status bits contain the channel number of the next
-        // channel to be converted.
-	u8 status_register = inb(AD_STATUS_REGISTER);
-	int status_register_int = 0x01 & (status_register >> 4);
-	u8 result_low = inb(AD_LOW_BYTE_AND_CH);
-	u8 result_high = inb(AD_HIGH_BYTE);
-
-        // return -1 if no A/D conversion hqs been completed since the
-        // last time the INT bit was cleared.
-        if (status_register_int != 1) {
-          return -1;
-        }
-
-	u16 result =  0x0FFF & ((result_high<<4) + (result_low>>4));
-	//printk("LOW = 0x%x\n", result_low);
-	//printk("LOW_D7D4 = 0x%x\n", result_low>>4);
-	//printk("HIGH = 0x%x\n", result_high);
-	//printk("regToRead = 0x%x\n", result);
-	//printk("valeur= %d\n", result);
-	//printk("channelAD= %d\n", result_low & 0x7); // on recup les 3 LSB
-        //printk("status register= 0x%x\n", status_register);
-	//printk("status_register_int = 0x%x\n", status_register_int);
-	return result;
-}//ReadAD
-
-double readAD_mVolt(void) {
-
-       return ((readAD()-2048)*1000)/205;
-
+    setChannel(channel);
+    // According to the datasheet, the MUX start/stop channel changes each time
+    // when you change the input range.
+    outb(range, AD_RANGE_CTRL);
+    // reset channel to prevent channel change after wrting in AD_RANGE_CTRL
+    // NOTE: disabled because start channel = stop channel in setChannel()
+    // setChannel(channel);
 }
+EXPORT_SYMBOL(ADRangeSelect);
+
+/**
+ * Read ADC register value
+ * on the channel selected by ADRangeSelect
+ *
+ * @return u16 voltage (0-4096)
+ */
+u16 readAD(void) {
+    // STATUS REGISTER
+    // D7: End Of Conversion:
+    //   1 Busy,
+    //   0 Ready for next conversion data from the previous
+    //     conversion is available in the A/D register
+    // D6: N/A
+    // D5: MUX: 0 8 Diff channels; 1 16 Single Ended Channels
+    // D4: INT Data valid
+    // D3-D0: CN3-CN0 When EOC = 0,
+    //
+    // these status bits contain the channel number of the next
+    // channel to be converted.
+    u8 status_register = inb(AD_STATUS_REGISTER);
+    int status_register_int = 0x01 & (status_register >> 4);
+    u8 result_low = inb(AD_LOW_BYTE_AND_CH);
+    u8 result_high = inb(AD_HIGH_BYTE);
+
+    // return -1 if no A/D conversion hqs been completed since the
+    // last time the INT bit was cleared.
+    if (status_register_int != 1) {
+      return -1;
+    }
+
+    u16 result =  0x0FFF & ((result_high<<4) + (result_low>>4));
+    //printk("LOW = 0x%x\n", result_low);
+    //printk("LOW_D7D4 = 0x%x\n", result_low>>4);
+    //printk("HIGH = 0x%x\n", result_high);
+    //printk("regToRead = 0x%x\n", result);
+    //printk("valeur= %d\n", result);
+    //printk("channelAD= %d\n", result_low & 0x7); // on recup les 3 LSB
+    //printk("status register= 0x%x\n", status_register);
+    //printk("status_register_int = 0x%x\n", status_register_int);
+    return result;
+}
+EXPORT_SYMBOL(readAD);
+
+/**
+ * Read voltage in mVolts
+ * on the channel selected by ADRangeSelect.
+ * @note: assuming range is +/-10V
+ *
+ * @return double voltage (mVolts)
+ */
+double readAD_mVolt(void) {
+    return ((readAD()-2048)*1000)/205;
+}
+EXPORT_SYMBOL(readAD_mVolt);
 
 /**
  * Uninstall driver module
  */
 void exit_3718(void) {
-  printk("uninstall 3718 driver\n");
+    printk("uninstall 3718 driver\n");
 }
-
-
-EXPORT_SYMBOL(init_3718);
 EXPORT_SYMBOL(exit_3718);
-EXPORT_SYMBOL(setChannel);
-EXPORT_SYMBOL(ADRangeSelect);
-EXPORT_SYMBOL(readAD);
-EXPORT_SYMBOL(readAD_mVolt);
 
 
 module_init(init_3718);
